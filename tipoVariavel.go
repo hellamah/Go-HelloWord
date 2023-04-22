f1a7e027b040f67b1de0cd06792ebd9465b32c31
Helamã Borges <helamaborges@gmail.com>
2023-04-22 07:29:06 -0300

    Maps e Slices

--- tipoVariavel.go
+++ tipoVariavel.go
@@ -34,4 +34,23 @@ func tipoVariavel() {
 	g, h, i := 2, false, "epa"
 
 	fmt.Println(g, h, i)
+
+	//Maps e slices
+	aprovados := make(map[int]string)
+
+	aprovados[12345678978] = "Maria"
+	aprovados[98765432100] = "Pedro"
+	aprovados[95135745682] = "Ana"
+
+	fmt.Println(aprovados)
+
+	for cpf, nome := range aprovados {
+		fmt.Printf("%s (CPF: %d)\n", nome, cpf)
+	}
+
+	fmt.Println(aprovados[95135745682])
+	delete(aprovados, 95135745682)
+
+	fmt.Println(aprovados[95135745682])
+
 }
