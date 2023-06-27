388b1f57a42b3d98477807120cdda2b41497854f
Helamã Borges <helamaborges@gmail.com>
2023-06-27 00:01:14 -0300

    Public e private

---
+++ pacote/reta.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"math"
+)
+
+type Ponto struct {
+	x float64
+	y float64
+}
+
+func catetos(a, b Ponto) (cx, cy float64) {
+	cx = math.Abs(b.x - a.x)
+	cy = math.Abs(b.y - a.y)
+	return
+}
+
+func Distancia(a, b Ponto) float64 {
+	cx, cy := catetos(a, b)
+	return math.Sqrt(math.Pow(cx, 2) + math.Pow(cy, 2))
+}
